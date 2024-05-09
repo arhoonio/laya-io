6c5a6961a08e01b1cfec5f0adef38869ad5e6738
Arjun Muralikrishnan <arjun.muralikrishnan1@gmail.com>
2024-05-09 10:36:09 -0500

    korvai helper works now yay wow

--- src/Algos/korvai-helper.py
+++ src/Algos/korvai-helper.py
@@ -6,6 +6,27 @@ def parseLine(line, section):
         ret.append(info["sollus"][kanakku][section][0])
     return ret
 
+def flatten_sollu(sollu):
+    ret_raw = []
+    for i in range(len(sollu["P"].keys())):
+        if "P" in sollu.keys():
+            for phrase in sollu["P"][i]: ret_raw.extend(phrase.split(' '))
+        if "M" in sollu.keys():
+            for phrase in sollu["M"][i]: ret_raw.extend(phrase.split(' '))
+        if "U" in sollu.keys():
+            for phrase in sollu["U"][i]: ret_raw.extend(phrase.split(' '))
+    return ret_raw
+
+def korvai_placed(sollu, symbols, gathi):
+    ret = ""
+    last_symbol = -1
+    flattened = flatten_sollu(sollu)
+    for i in range(0, len(flattened), gathi):
+        if last_symbol+3 > len(symbols):last_symbol=-1
+        else: last_symbol+=1
+        ret+=f"({symbols[last_symbol]})"+ ' ' + ' '.join(flattened[i:i+gathi])+'\n'
+    return ret
+
 thalam = input("thalam: ")
 jaathi = int(input("jaathi (# aksharams in laghu): "))
 gathi = int(input("gathi (# subdivisions per aksharam): "))
@@ -30,6 +51,17 @@ info = {
         "matya" : gathi*(2+jaathi),
         "jhampa": gathi*(jaathi + 3),
         "dhruva": gathi*(jaathi + 2 + jaathi)
+    },
+    "prefixes" : {
+        1: "eka",
+        3: "tisra",
+        4: "chaturasra",
+        5: "kanda",
+        6: "tisra",
+        7: "misra",
+        8: "chaturasra",
+        9: "sankeerna",
+        10: "kanda"
     },
     "sollus" : {
         '1': {"P": ["tha", "dhi", "thom", "nam"],
@@ -59,6 +91,9 @@ info = {
         '9':  {"P": ["tha ka dhi mi tha ka tha ki ta", "tha - dheem - tha dhin gi na thom", "thari thari kita thaka thari kita kita thaka naka", "thom - thom - thom - thom - tha"],
               "M": ["tha ka dhi mi tha ka tha ki ta", "tha - dheem - tha dhin gi na thom", "thari thari kita thaka thari kita kita thaka naka", "thom - thom - thom - thom - tha"],
               "U": ["tha - dheem - tha dhin gi na thom", "tha ka dhi mi tha ka tha ki ta", "thari thari kita thaka thari kita kita thaka naka", "thom - thom - thom - thom - tha"]},
+        '10':  {"P": ["tha- tha- thari thari kita thaka thaka thari kita thaka", "ki ta tha ka tha ri ki ta thom -", "tha ri tha ri ki ta ki ta kum", "thari kita kita thaka naka"],
+              "M": ["ki ta tha ka tha ri ki ta thom -", "tha - dhi - thari thari kita thaka thaka thari kita thaka", "tha ri tha ri ki ta ki ta kum", "thari kita kita thaka naka"],
+              "U": ["tha ki ta dheem - tha dhin gi na thom", "ki ta tha ka tha ri ki ta thom -", "tha - dhi - thari thari kita thaka thaka thari kita thaka", "tha ri tha ri ki ta ki ta kum", "thari kita kita thaka naka"]},
         
         '-': {"P": ["-"],
               "M":["-"],
@@ -84,34 +119,58 @@ info = {
         '(7)': {"P": ["dhin - dhin - thaan - gu", "dhin - dhin - dheen - gu"],
               "M":["dhin - dhin - thaan - gu", "dhin - dhin - dheen - gu"],
               "U": ["dhin - dhin - thaan - gu", "dhin - dhin - dheen - gu"]},
+        '(8)': {"P": ["tham - - - tham - - -", "dheem - - - dheem - - -"],
+              "M":["tham - - - tham - - -", "dheem - - - dheem - - -"],
+              "U": ["tham - - - tham - - -", "dheem - - - dheem - - -"]},
     },
 }
 
-korvai = {}
+korvai = {'total':0}
 sollu = {}
 with open(sys.argv[1]) as f:
     section = "P"
+    count = {
+        "P": -1,
+        "M":-1,
+        "U":-1
+    }
     for line in f:
         if "P" in line:
             section = "P"
-            korvai[section] = []
-            sollu[section] = []
+            count[section]+=1
+            if section not in korvai.keys(): korvai[section] = {}
+            if section not in sollu.keys(): sollu[section] = {}
+            if count[section] not in korvai[section].keys(): korvai[section][count[section]] = []
+            if count[section] not in sollu[section].keys(): sollu[section][count[section]] = []
         elif "U" in line:
             section = "U"
-            korvai[section] = []
-            sollu[section] = []
+            count[section]+=1
+            if section not in korvai.keys(): korvai[section] = {count[section]:[]}
+            if section not in sollu.keys(): sollu[section] = {count[section]:[]}            
+            if count[section] not in korvai[section].keys(): korvai[section][count[section]] = []
+            if count[section] not in sollu[section].keys(): sollu[section][count[section]] = []
         elif "M" in line:
             section = "M"
-            korvai[section] = []
-            sollu[section] = []
+            count[section]+=1
+            if section not in korvai.keys(): korvai[section] = {count[section]:[]}
+            if section not in sollu.keys(): sollu[section] = {count[section]:[]}            
+            if count[section] not in korvai[section].keys(): korvai[section][count[section]] = []
+            if count[section] not in sollu[section].keys(): sollu[section][count[section]] = []
         else:
             formatted_line = line.strip().split(' ')
-            korvai[section] += line.strip().split(' ')
-            sollu[section] += parseLine(formatted_line, section)
-
-print(info["thalam-symbols"][thalam])
-print(info["thalam-aks"][thalam])
-print("---POORVANGAM---")
-for bol in sollu['P']: print(bol)
-print("---UTTARANGAM---")
-for bol in sollu['U']: print(bol)
+            valueList = [int(string) for string in line.replace('(','').replace(')','').strip().split(' ')]
+            korvai[section][count[section]] += valueList
+            korvai["total"] += sum(valueList)
+            sollu[section][count[section]] += parseLine(formatted_line, section)
+print()
+print("---THALAM INFO---")
+print(f"{info['prefixes'][jaathi]} jaathi {thalam} thalam ({info['prefixes'][gathi]} gathi)")
+print("layout:", end=' ')
+for symbol in info["thalam-symbols"][thalam]: print(symbol, end=' ')
+print()
+print("total aksharams in one avarthanam:", info["thalam-aks"][thalam])
+print()
+print("---KORVAI INFO---")
+print("total aksharams:", korvai["total"])
+print()
+print(korvai_placed(sollu=sollu, symbols=info["thalam-symbols"][thalam], gathi=gathi))
