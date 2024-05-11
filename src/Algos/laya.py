17f25f896e50090da3e360f5edf29f6e9aeace66
Arjun Muralikrishnan <arjun.muralikrishnan1@gmail.com>
2024-05-11 00:07:45 -0500

    its so good it works ahhhh

--- src/Algos/laya.py
+++ src/Algos/laya.py
@@ -105,19 +105,19 @@ info = {
         '3': ["tha ki ta", "ku ku tha"],
         '4': ["tha ka dhi na", "ki ta tha ka", "tha ka dhi mi"],
         '5': ["tha dhin gi na thom", "tha ka tha ki ta", "tha dhin gi na thom", "thari kita kita thaka naka", "kita thaka thari kita thom-", "thari thari kita kita kum-"],
-        '6':  ["tham- kita thaka thari kita thaka", "tha dhin - gi na thom", "tham- kita thaka thom- kita thom-",],
-        '7':  ["tha ka dhi mi tha ki ta", "tha - dhin - gi na thom", "tham- kita thaka thaka thari kita thaka", "kita thaka thari kita kita thaka naka",],
+        '6':  ["tha dhin - gi na thom", "tham- kita thaka thari kita thaka", "tham- kita thaka thom- kita thom-",],
+        '7':  ["tha - dhin - gi na thom", "tha ka dhi mi tha ki ta", "tham- kita thaka thaka thari kita thaka", "kita thaka thari kita kita thaka naka",],
         '8':  ["thi- - tham- kita thaka thari kita thaka", "tha dheem - tha dhin gi na thom", "tha ka dhi mi tha ka ja nu", "tham - ki ta tha ka ja nu"],
         '9':  ["tha ka dhi mi tha ka tha ki ta", "tha - dheem - tha dhin gi na thom", "thari thari kita thaka thari kita kita thaka naka", "thom - thom - thom - thom - tha"],
-        '10':  ["tha- tha- thari thari kita thaka thaka thari kita thaka", "ki ta tha ka tha ri ki ta thom -", "tha ri tha ri ki ta ki ta kum", "thari kita kita thaka naka"],
+        '10':  ["tha ki ta dheem - tha dhin gi na thom", "tha - dhin - gi - na - thom -", "ki ta tha ka tha ri ki ta thom -",  "tha ri tha ri ki ta ki ta kum", "thari kita kita thaka naka", "tha- tha- thari thari kita thaka thaka thari kita thaka",],
         
         '-': ["-"],
         '(1)': ["-"],
         '(2)': ["tham -", "dheem -", "dhin -", "dhi -", "- -"],
         '(3)': ["thaan - gu", "dheen - gu", "tham - -", "- - -"],
-        '(4)': ["dhin - tha -", "tham - tham -", "dheem - dheem -", "- - - -"],
+        '(4)': ["tham - - -", "tham - tham -", "dheem - dheem -", "- - - -"],
         '(5)': ["dhin - thaan - gu", "dhin - dheen - gu", "- - - - -"],
-        '(6)': ["thaan - gu thaan - gu", "dheen - gu dheen - gu", "tham - tham - tham -"],
+        '(6)': ["dheen - - - gu -","thaan - gu thaan - gu", "dheen - gu dheen - gu", "tham - tham - tham -"],
         '(7)': ["dhin - dhin - thaan - gu", "dhin - dhin - dheen - gu"],
         '(8)': ["tham - - - tham - - -", "dheem - - - dheem - - -"],
     },
@@ -143,15 +143,15 @@ print()
 print(korvai_placed(sollu=solluList, symbols=info["thalam-symbols"][thalam], gathi=gathi))
 print("total aksharams:",len(solluList))
 
-with open(f'{sys.argv[1][:-4]}-results.txt', 'x') as f:
-      f.write(f"{info['prefixes'][jaathi]} jaathi {thalam} thalam ({info['prefixes'][gathi]} gathi)\n")
-      f.write('layout: ')
-      for symbol in info["thalam-symbols"][thalam]: f.write(symbol+' ')
-      f.write('\n')
-      f.write(f"total aksharams in one avarthanam: {info['thalam-aks'][thalam]} \n")
-      f.write('\n')
-      f.write(korvai_placed(sollu=solluList, symbols=info['thalam-symbols'][thalam], gathi=gathi))
-      f.write('\n')
-      f.write(f"total aksharams: {len(solluList)}")
+# with open(f'{sys.argv[1][:-4]}-results.txt', 'x') as f:
+#       f.write(f"{info['prefixes'][jaathi]} jaathi {thalam} thalam ({info['prefixes'][gathi]} gathi)\n")
+#       f.write('layout: ')
+#       for symbol in info["thalam-symbols"][thalam]: f.write(symbol+' ')
+#       f.write('\n')
+#       f.write(f"total aksharams in one avarthanam: {info['thalam-aks'][thalam]} \n")
+#       f.write('\n')
+#       f.write(korvai_placed(sollu=solluList, symbols=info['thalam-symbols'][thalam], gathi=gathi))
+#       f.write('\n')
+#       f.write(f"total aksharams: {len(solluList)}")
 
 korvai_pd_export(sollu=solluList, symbols=info["thalam-symbols"][thalam], gathi=gathi)
